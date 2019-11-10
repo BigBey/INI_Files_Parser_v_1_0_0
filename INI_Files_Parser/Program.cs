e36024d239e4a7c07eca7d65dca64c1b2dedd9df
BigBey <bey.sviatoslav@yandex.ru>
2019-11-10 20:22:24 +0300

    read iniFile

--- INI_Files_Parser/Program.cs
+++ INI_Files_Parser/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace INI_Files_Parser
+﻿using INI_Files_Parser.Parser;
+
+namespace INI_Files_Parser
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            //
+            IniFile iniFile = new IniFile("C:\\Users\\zenbook\\RiderProjects\\INI_Files_Parser\\INI_Files_Parser\\Test\\test.ini");
+            iniFile.ReadSection("GeneralConfiguration");
         }
     }
 }
