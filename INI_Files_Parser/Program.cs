e88f042c05d7a716702fc8c94115df54f8078934
BigBey <bey.sviatoslav@yandex.ru>
2019-11-10 21:17:49 +0300

    add .ini file and reading

--- INI_Files_Parser/Program.cs
+++ INI_Files_Parser/Program.cs
@@ -1,4 +1,5 @@
-﻿using INI_Files_Parser.Parser;
+﻿using System;
+using INI_Files_Parser.Parser;
 
 namespace INI_Files_Parser
 {
@@ -6,8 +7,37 @@ namespace INI_Files_Parser
     {
         public static void Main(string[] args)
         {
-            IniFile iniFile = new IniFile("C:\\Users\\zenbook\\RiderProjects\\INI_Files_Parser\\INI_Files_Parser\\Test\\test.ini");
-            iniFile.ReadSection("GeneralConfiguration");
+            var ini = new IniFile(@"C:\Users\zenbook\RiderProjects\INI_Files_Parser_v_1_0_0\INI_Files_Parser\Test\test.ini");
+
+            var sections = ini.ReadSections();
+            
+            int cnt = 1;
+            foreach (string sectionName in sections)
+            {
+                Console.WriteLine("#" + cnt.ToString() + " - " + sectionName);
+
+                var keys = ini.ReadSectionKeys(sectionName);
+                foreach (string keyName in keys)
+                {
+                    Console.WriteLine(keyName + " = " + ini.ReadString(sectionName, keyName, "UNKNOWN"));
+                }
+                Console.WriteLine("");
+
+                cnt++;
+            }
+
+            int i = ini.ReadInteger("GeneralConfiguration", "setUpdate", 0);
+            Console.WriteLine("Integer=" + i.ToString());
+
+            long i64 = ini.ReadInteger64("valueTests", "integer64", 0);
+            Console.WriteLine("Integer64=" + i64.ToString());
+
+            string s = ini.ReadString("Users", "ricky", "");
+            Console.WriteLine("String=" + s);
+
+            
+
+            Console.ReadLine();
         }
     }
 }
