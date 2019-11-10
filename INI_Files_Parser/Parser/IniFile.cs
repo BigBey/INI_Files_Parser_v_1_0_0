984a7c74353c54e7b4dd6271aa9aabf4c8f74161
BigBey <bey.sviatoslav@yandex.ru>
2019-11-10 19:49:42 +0300

    delete all another functions

--- INI_Files_Parser/Parser/IniFile.cs
+++ INI_Files_Parser/Parser/IniFile.cs
@@ -67,28 +67,6 @@ namespace INI_Files_Parser.Parser
                 }
             }
         }
-
-        private void _Save()
-        {
-            TextWriter file = new StreamWriter(_iniFileFilename, false, Encoding.GetEncoding("iso-8859-1"));
-
-            foreach (var section in iniContent)
-            {
-                file.WriteLine("[" + section.sectionName + "]");
-
-                var keys = section.ReadSectionKeys();
-
-                foreach (var key in keys)
-                {
-                    var value = section[key];
-                    file.WriteLine(key + "=" + value);
-                }
-                file.WriteLine("");
-            }
-
-            file.Close();
-        }
-
         // constructor
         
         /// <summary>
@@ -112,53 +90,7 @@ namespace INI_Files_Parser.Parser
             get { return _iniFileFilename; }
             set { _iniFileFilename = value; }
         }
-
-        /// <summary>
-        /// Call DeleteKey to erase an INI file entry. If the key is the only one in the section, the section will be deleted as well.
-        /// </summary>
-        /// <param name="sectionName">Name of an INI file section</param>
-        /// <param name="keyName">Name of the Key to delete from the section</param>
-        public void DeleteKey(string sectionName, string keyName)
-        {
-            for (int i = 0; i < iniContent.Count - 1; i++)
-            {
-                var sec = GetSection(sectionName);
-                if (sec != null)
-                {
-                    string value;
-                    if (sec.TryGetValue(keyName, out value))
-                    {
-                        if (sec.Count == 1)
-                        {
-                            // only key in the section, delete the section instead.
-                            EraseSection(sectionName);
-                        }
-                        else
-                        {
-                            sec.Remove(keyName);
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Call EraseSection to remove a section, all its keys, and their data values from an INI file.
-        /// </summary>
-        /// <param name="sectionName">Section identifies the INI file section to remove</param>
-        public void EraseSection(string sectionName)
-        {
-            for (int i = 0; i < iniContent.Count - 1; i++)
-            {
-                if (iniContent[i].sectionName.ToUpper() == sectionName.ToUpper())
-                {
-                    iniContent.RemoveAt(i);
-                    return;
-                }
-            }
-        }
-
+        
         /// <summary>
         /// Get the IniSection object that holds section information.
         /// </summary>
@@ -224,41 +156,7 @@ namespace INI_Files_Parser.Parser
             }
             return result;
         }
-
-        /// <summary>
-        /// Call WriteString to write a string value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteString creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the string value to write.</param>
-        public void WriteString(string sectionName, string keyName, string value)
-        {
-            var section = GetSection(sectionName);
-            if (section == null)
-            {
-                section = new IniSection(sectionName);
-                iniContent.Add(section);
-            }
-
-            if (section.ContainsKey(keyName))
-            {
-                section[keyName] = value;
-            }
-            else
-            {
-                section.Add(keyName, value);
-            }
-        }
-
-        /// <summary>
-        /// Saves the Inifile to the file specified in filename.
-        /// </summary>
-        public void Save()
-        {
-            _Save();
-        }
+        
 
         /// <summary>
         /// Loads the Inifile specified in filename.
@@ -312,71 +210,7 @@ namespace INI_Files_Parser.Parser
             }
             return false;
         }
-
-        /// <summary>
-        /// Call WriteBool to write a boolean value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteBool creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the boolean value to write.</param>
-        public void WriteBool(string sectionName, string keyName, bool value)
-        {
-            // Delphi compatible
-            // 0 - False
-            // 1 - True
-            string valueStr;
-            if (value)
-            {
-                valueStr = "1";
-            }
-            else
-            {
-                valueStr = "0";
-            }
-            WriteString(sectionName, keyName, valueStr);
-        }
-
-        /// <summary>
-        /// Call ReadBool to read a boolean value from an INI file.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contains the desired key.</param>
-        /// <param name="keyName">keyName is the name of the key from which to retrieve the value.</param>
-        /// <param name="defaultValue">defaultValue is the boolean value to return if the section or key does not exists</param>
-        /// <returns></returns>
-        public bool ReadBool(string sectionName, string keyName, bool defaultValue)
-        {
-            if (KeyExists(sectionName, keyName))
-            {
-                if (ReadString(sectionName, keyName, "0") == "1")
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return defaultValue;
-            }
-        }
-
-        /// <summary>
-        /// Call WriteInteger to write a integer (int) value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteInteger creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the integer (int) value to write.</param>
-        public void WriteInteger(string sectionName, string keyName, int value)
-        {
-            WriteString(sectionName, keyName, value.ToString());
-        }
-
+        
         /// <summary>
         /// Call ReadInteger to read a integer (int) value from an INI file.
         /// </summary>
@@ -393,19 +227,6 @@ namespace INI_Files_Parser.Parser
 
             int result = Convert.ToInt32(ReadString(sectionName, keyName, "-1"));
             return result;
-        }
-
-        /// <summary>
-        /// Call WriteInteger64 to write a 64bit integer (long) value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteInteger64 creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the 64bit integer (long) value to write.</param>
-        public void WriteInteger64(string sectionName, string keyName, long value)
-        {
-            WriteString(sectionName, keyName, value.ToString());
         }
 
         /// <summary>
@@ -427,50 +248,6 @@ namespace INI_Files_Parser.Parser
         }
 
         /// <summary>
-        /// Call WriteShort to write a 16bit integer (short) value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteShort creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the 16bit integer (short) value to write.</param>
-        public void WriteShort(string sectionName, string keyName, short value)
-        {
-            WriteString(sectionName, keyName, value.ToString());
-        }
-
-        /// <summary>
-        /// Call ReadShort to read a 16bit integer (short) value from an INI file.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contains the desired key.</param>
-        /// <param name="keyName">keyName is the name of the key from which to retrieve the value.</param>
-        /// <param name="defaultValue">defaultValue is the 16bit integer (short) value to return if the section or key does not exists</param>
-        /// <returns></returns>
-        public short ReadShort(string sectionName, string keyName, short defaultValue)
-        {
-            if (!KeyExists(sectionName, keyName))
-            {
-                return defaultValue;
-            }
-
-            short result = Convert.ToInt16(ReadString(sectionName, keyName, "-1"));
-            return result;
-        }
-
-        /// <summary>
-        /// Call WriteFloat to write a double value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteShort creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the double value to write.</param>
-        public void WriteFloat(string sectionName, string keyName, double value)
-        {
-            WriteString(sectionName, keyName, value.ToString());
-        }
-
-        /// <summary>
         /// Call ReadFloat to read a double value from an INI file.
         /// </summary>
         /// <param name="sectionName">sectionName identifies the section in the file that contains the desired key.</param>
@@ -485,37 +262,6 @@ namespace INI_Files_Parser.Parser
             }
 
             double result = double.Parse(ReadString(sectionName, keyName, "0"));
-            return result;
-        }
-
-        /// <summary>
-        /// Call WriteDateTime to write a DateTime value to an INI file.
-        /// Attempting to write a data value to a nonexistent section or attempting to write data to a nonexistent key are not errors. 
-        /// In these cases, WriteDateTime creates the section and key and sets its initial value to Value.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contain the key to which to write.</param>
-        /// <param name="keyName">keyName is the name of the key for which to set a value.</param>
-        /// <param name="value">value is the DateTime value to write.</param>
-        public void WriteDateTime(string sectionName, string keyName, DateTime value)
-        {
-            WriteString(sectionName, keyName, value.ToString());
-        }
-
-        /// <summary>
-        /// Call ReadDateTime to read a double value from an INI file.
-        /// </summary>
-        /// <param name="sectionName">sectionName identifies the section in the file that contains the desired key.</param>
-        /// <param name="keyName">keyName is the name of the key from which to retrieve the value.</param>
-        /// <param name="defaultValue">defaultValue is the DateTime value to return if the section or key does not exists</param>
-        /// <returns></returns>
-        public DateTime ReadDateTime(string sectionName, string keyName, DateTime defaultValue)
-        {
-            if (!KeyExists(sectionName, keyName))
-            {
-                return defaultValue;
-            }
-
-            DateTime result = DateTime.Parse(ReadString(sectionName, keyName, "0"));
             return result;
         }
     }
